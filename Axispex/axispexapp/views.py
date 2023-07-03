868a3db180540d2d73627bfb4d3f8041109e1d16
Axispex <axispexlabs@gmail.com>
2023-07-03 15:10:39 +0530

    2nd pagecommit1

--- Axispex/axispexapp/views.py
+++ Axispex/axispexapp/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
+    return render(request, 'axispex/index.html')
+
+
+def loginpage(request):
     return render(request, 'axispex/loginpage.html')
